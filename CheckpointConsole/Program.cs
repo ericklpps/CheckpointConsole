60499bc5ad0cf3201f00400330774ea240f2651e
ericklpps <ericklpsilva@gmail.com>
2024-09-24 19:14:25 -0300

    Adicionar arquivos de projeto.

---
+++ CheckpointConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome completo: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua data de nascimento (dd/mm/aaaa): ");
+        DateTime dataNascimento;
+        bool dataValida = DateTime.TryParse(Console.ReadLine(), out dataNascimento);
+
+        if (!dataValida)
+        {
+            Console.WriteLine("Data de nascimento inválida. Tente novamente.");
+            return;
+        }
+
+        DateTime dataAtual = DateTime.Now;
+        
+        int idade = dataAtual.Year - dataNascimento.Year;
+
+        if (dataAtual < dataNascimento.AddYears(idade))
+        {
+            idade--;
+        }
+
+        Console.WriteLine($"{nome}, {idade} anos.");
+    }
+}
+
